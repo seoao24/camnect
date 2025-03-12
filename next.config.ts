3d03c597a1a13cc02b0ff72c074963470ff82a5e
Giang Seo Ao <seoao24@gmail.com>
2025-03-12 09:48:51 +0700

    fix bug

--- next.config.ts
+++ next.config.ts
@@ -2,35 +2,6 @@ import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*", // Hoặc chỉ định domain cụ thể
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-        ],
-      },
-    ];
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://183.81.33.169:5000/:path*", // Thay URL của API thật vào đây
-      },
-    ];
-  },
 };
 
 export default nextConfig;
